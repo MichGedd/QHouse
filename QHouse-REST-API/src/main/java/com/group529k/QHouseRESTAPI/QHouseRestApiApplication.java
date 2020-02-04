4a18ff4108aa39a04880b74079188903126eb316
MichGedd <michaelgeddes555@gmail.com>
2020-02-04 16:20:48 -0500

    Added spring project

---
+++ QHouse-REST-API/src/main/java/com/group529k/QHouseRESTAPI/QHouseRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.group529k.QHouseRESTAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QHouseRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QHouseRestApiApplication.class, args);
+	}
+
+}
---
+++ QHouse-REST-API/src/test/java/com/group529k/QHouseRESTAPI/QHouseRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group529k.QHouseRESTAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QHouseRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
