29c6c6aa905652d9f26fb8accafd8ff4121d213c
MichGedd <michaelgeddes555@gmail.com>
2020-03-03 15:30:55 -0500

    Added login features

---
+++ QHouseMobile/QHouseMobile/Classes/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QHouseMobile.Classes
+{
+    class Student
+    {
+        private int id;
+        private string email;
+        private string name;
+
+        public int Id { get => id; set => id = value; }
+        public string Email { get => email; set => email = value; }
+        public string Name { get => name; set => name = value; }
+    }
+}
---
+++ QHouseMobile/QHouseMobile/Views/Login.xaml.cs
@@ -0,0 +1,92 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace QHouseMobile
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        private string email, password;
+        static readonly HttpClient httpClient = new HttpClient();
+
+        bool isStudent = true;
+
+        public Login()
+        {
+            InitializeComponent();
+            httpClient.BaseAddress = new Uri(ApplicationResources.BaseURI);
+        }
+
+        void EmailChanged(object sender, EventArgs e)
+        {
+            email = ((Entry)sender).Text;
+        }
+
+        void PasswordChanged(object sender, EventArgs e)
+        {
+            password = ((Entry)sender).Text;
+        }
+
+        void OnSwitchToggled(object sender, ToggledEventArgs e)
+        {
+            isStudent = e.Value;
+        }
+
+        async void OnLoginClicked(object sender, EventArgs e)
+        {
+            if (!String.Equals(password, "") && !String.Equals(email, ""))
+            {
+                var values = new Dictionary<String, String>
+                {
+                    {"email", email },
+                    {"password", password }
+                };
+
+                string payload = JsonConvert.SerializeObject(values);
+                var content = new StringContent(payload, Encoding.UTF8, "application/json");
+
+                try
+                {
+                    string apiEndpoint = isStudent ? "student/login" : "landlord/login";
+
+                    var response = await httpClient.PostAsync(apiEndpoint, content);
+                    var id = await response.Content.ReadAsStringAsync();
+
+                    Debug.WriteLine(id);
+
+                    if (id.Equals("false"))
+                    {
+                        throw new ArgumentException("Student with that email doesnt exist!");
+                    }
+
+                    if (App.Current.Properties.ContainsKey("id"))
+                        App.Current.Properties["id"] = id;
+                    else
+                        App.Current.Properties.Add("id", id);
+
+                    string userType = isStudent ? "student" : "landlord";
+
+                    if (App.Current.Properties.ContainsKey("type"))
+                        App.Current.Properties["type"] = userType;
+                    else
+                        App.Current.Properties.Add("type", userType);
+
+
+                } catch (Exception exception)
+                {
+                    Debug.WriteLine(exception);
+                }
+            }
+        }
+
+    }
+}
