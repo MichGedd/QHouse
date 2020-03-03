e9602b253e3436cc822d0d2a88676abde211bb4e
MichGedd <michaelgeddes555@gmail.com>
2020-03-03 18:20:39 -0500

    Added backend API call for adding houses

---
+++ QHouse-REST-API/src/main/java/com/group529k/QHouseRESTAPI/repository/HouseRepository.java
@@ -0,0 +1,7 @@
+package com.group529k.QHouseRESTAPI.repository;
+
+import com.group529k.QHouseRESTAPI.entity.House;
+import org.springframework.data.repository.CrudRepository;
+
+public interface HouseRepository extends CrudRepository<House, Integer> {
+}
