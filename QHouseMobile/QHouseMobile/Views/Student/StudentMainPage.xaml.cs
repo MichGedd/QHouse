fd07cd02b71b3409e506473e849670da90bb3a18
MichGedd <michaelgeddes555@gmail.com>
2020-03-11 17:10:48 -0400

    Created skeleton spacing and layout of the student main page

--- QHouseMobile/QHouseMobile/Views/Student/StudentMainPage.xaml.cs
+++ QHouseMobile/QHouseMobile/Views/Student/StudentMainPage.xaml.cs
@@ -12,9 +12,58 @@ namespace QHouseMobile
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class StudentMainPage : ContentPage
     {
+        private string houseNumBedrooms;
+        private string houseNumBathrooms;
+        private string houseRent;
+        private string houseAddress;
+
+        public string HouseNumBedrooms
+        {
+            get { return houseNumBedrooms; }
+            set
+            {
+                houseNumBedrooms = value;
+                OnPropertyChanged(nameof(HouseNumBedrooms));
+            }
+        }
+        public string HouseNumBathrooms
+        {
+            get { return houseNumBathrooms; }
+            set
+            {
+                houseNumBathrooms = value;
+                OnPropertyChanged(nameof(HouseNumBathrooms));
+            }
+        }
+        public string HouseRent
+        {
+            get { return houseRent; }
+            set
+            {
+                houseRent = value;
+                OnPropertyChanged(nameof(HouseRent));
+            }
+        }
+        public string HouseAddress
+        {
+            get { return houseAddress; }
+            set
+            {
+                houseAddress = value;
+                OnPropertyChanged(nameof(HouseAddress));
+            }
+        }
+
+
+
         public StudentMainPage()
         {
             InitializeComponent();
+            BindingContext = this;
+            HouseNumBedrooms = "4";
+            HouseNumBathrooms = "4";
+            HouseRent = "$1500.00";
+            HouseAddress = "123 Street St";
         }
     }
 }
